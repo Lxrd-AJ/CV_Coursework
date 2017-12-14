feca67fdc54cfa4d449f89f43bcc3adf7c81f7dc
phisey <phil.seybold@gmail.com>
2017-12-14 04:23:04 +0000

    Finished old TODOS in knn.py; need to increasy accuracy of kNN

--- SceneRecognition/knn.py
+++ SceneRecognition/knn.py
@@ -6,6 +6,11 @@ from imutils import paths
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+from sklearn.model_selection import ShuffleSplit
+
 
 """
 Feature extracting function
@@ -13,25 +18,31 @@ Extracts features from an image and returns it which can
 be used to train a classifier
 """
 def feature_vector(image, size=(16,16)):
-    return cv2.resize(image, size).flatten()
+    img = cv2.resize(image, size)
+    return img
 
 """
 Calculates the histogram for a vector
 """
-def histogram(vec, bin_size=8):
+def histogram(vec, bin_size=16):
     hist = cv2.calcHist([vec], [0], None, [bin_size], [0,256])
     hist = hist.flatten()
-    mean = cv2.mean(hist)[0]
-    mag = np.sqrt(hist.dot(hist))
-    hist[:] = [x - mean for x in hist]
-    hist[:] = [x / mag for x in hist]
+    hist = cv2.normalize( hist , hist )
+
+    # Invert commented and uncommented part to enable/disable histogram or zero mean&unit length; seems to decrease accuracy of test set
+    #hist = vec
+    #mean = cv2.mean(hist)[0]
+    #hist = hist.flatten()
+    #hist = hist-mean
+    #mag = np.sqrt(hist.dot(hist))
+    #hist = hist/mag
     return hist
 
 """
 Helper function that simply returns the label or class of the image given its path
 """
 def extract_label( img_path ):
-    return img_path.split( os.path.sep )[2]
+    return img_path.split( os.path.sep )[1]
 
 """
 - Train the kNN Classifier, this is achieved in the following simple steps
@@ -43,11 +54,10 @@ def extract_label( img_path ):
 """
 if __name__ == "__main__":
     img_paths = paths.list_images("./training")
-    # img_paths = list(img_paths)[:100]
     feature_vecs = []
     labels = []
     for (i,img_path) in enumerate(img_paths):
-        image = cv2.imread(img_path)
+        image = cv2.imread(img_path,0)
         ftv = feature_vector( image )
         hist = histogram( ftv )
         label = extract_label(img_path)
@@ -55,31 +65,68 @@ if __name__ == "__main__":
         feature_vecs.append(hist)
         labels.append(label)
 
-        print("{:} as label {:}".format(img_path,label))
+        print("{:} as label {:}".format(img_path,label,))
     
-    #TODO: Shuffle the datat
-    #TODO: Parameter tuning for the kNN classifier i.e GridSearch http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-    model = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
+    # shuffle the data using a cross-validation shuffleSplitter
+    cv = ShuffleSplit(n_splits=50, test_size=0.3, random_state=None)
+    
+    # creating odd list of K for KNN
+    myList = list(range(1,100))
+    # subsetting just the odd ones
+    neighbors = list(filter(lambda x: x % 2 != 0, myList))
+    # empty list that will hold cv scores
+    cv_scores = []
+        
+    # perform cross validation
+    for k in neighbors:
+        model = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
+        scores = cross_val_score(model, feature_vecs, labels, cv=cv, scoring='accuracy')
+        cv_scores.append(scores.mean())
+        print("Finished CV: {:.2f}%".format(k))
+    # changing to misclassification error
+    MSE = [1 - x for x in cv_scores]
+
+    # determining best k
+    optimal_k = neighbors[MSE.index(min(MSE))]
+    print ("The optimal number of neighbors is %d" % optimal_k)
+
+    # plot misclassification error vs k
+    plt.plot(neighbors, MSE)
+    plt.xlabel('Number of Neighbors K')
+    plt.ylabel('Misclassification Error')
+    plt.show()
+
+    # model.fit( feature_vecs, labels )
+    model = KNeighborsClassifier(n_neighbors=optimal_k, n_jobs=-1,algorithm='kd_tree')
     model.fit( feature_vecs, labels )
-    #TODO: Perform cross validation on the dataset
-    acc = model.score( feature_vecs, labels )
+    acc = model.score(feature_vecs, labels )
     print("kNN accuracy: {:.2f}%".format(acc * 100))
-    
-    #Making predictions
-    img_paths = paths.list_images("./testing")
+
+    # making predictions
+    img_paths = paths.list_images("./testing_labelled")
+
+    feature_vecs = []
+    labels = []
     
     for img_path in img_paths:
-        image = cv2.imread(img_path)
+        image = cv2.imread(img_path,0)
         ftv = feature_vector( image )
         hist = histogram( ftv )
         label = extract_label(img_path)
+        feature_vecs.append(hist)
+        labels.append(label)
 
-        prediction = model.predict( [hist] )[0]
-        with open('./run1.txt','a') as f:
-            entry = "{:} {:}\n".format(label, prediction)
-            f.write(entry)
-            print(entry)
-    
-    #TODO: Generate plots for use in report
-    
+        # uncomment to predict and write into file; suggested after kNN accuracy > 10% for test set
+
+        #prediction = model.predict( [hist] )[0]
+        #with open('./run1.txt','a') as f:
+        #    entry = "{:} {:}\n".format(label, prediction)
+        #    f.write(entry)
+        #    print(entry)   
+
+    # TODO: Improve the accuracy to at least 10% for the test set
+    acc = model.score(feature_vecs, labels )
+    print("kNN accuracy: {:.2f}%".format(acc * 100))
+
+
         
