49042fa7b10c24e4126564758a670f9069aec86e
phisey <phil.seybold@gmail.com>
2017-12-14 19:39:13 +0000

    changed feature extractor as described in assigment

--- SceneRecognition/ensemble_linear.py
+++ SceneRecognition/ensemble_linear.py
@@ -2,6 +2,7 @@ import cv2
 import imutils 
 import os 
 import numpy as np 
+import random
 from imutils import paths
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import paired_distances
@@ -10,6 +11,37 @@ from functools import reduce
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.utils import shuffle
+from sklearn.model_selection import GridSearchCV
+
+
+
+def feature_vector(vec, bin_size):
+    hist = cv2.calcHist([vec], [0], None, [bin_size], [0,bin_size^2]).flatten()
+    return cv2.normalize( hist , hist )
+
+# TODO: change p_s back to 8 and step back to 4
+def features( image ):
+    # Patch size (quadratic)
+    p_s = 3
+    step = 50
+    p_c = np.floor(p_s/2)
+    img_s = np.size(image)
+    desc=[]
+    x=y=0
+    while x < img_s- p_c:
+        while y < img_s-p_c:
+            patch = image[x:x+p_s, y:y+p_s]
+            desc.append(feature_vector(patch,p_s))
+            y+=step
+        x+=step
+    # Shift extractor:
+    #sift_extractor = cv2.xfeatures2d.SIFT_create()
+    #desc = sift_extractor.detectAndCompute( image,None )[1]
+
+    #TODO: Remove later, temp hack to make it run faster
+    # The clusters would have to be recalculated if this is changed
+    #desc = desc[:,50:60]
+    return desc
 
 def build_dictionary( dir_name ):
     img_dict = {}
@@ -21,7 +53,7 @@ def build_dictionary( dir_name ):
         img_dict[word] = []
         for file in files:
             img_file = os.path.join( name,file )
-            img = cv2.imread( img_file )
+            img = cv2.imread( img_file,0 )
             if img is None:
                 print("Not an image " + img_file)
                 continue
@@ -29,35 +61,36 @@ def build_dictionary( dir_name ):
             count += 1
     return (img_dict, count)
 
-#TODO: Don't use SIFT for the features anymore, use the method specified in assignment
-def features( image ):
-    sift_extractor = cv2.xfeatures2d.SIFT_create()
-    desc = sift_extractor.detectAndCompute( image,None )[1]
-    #TODO: Remove later, temp hack to make it run faster
-    # The clusters would have to be recalculated if this is changed
-    desc = desc[:,50:60]
-    return desc
-
 def build_features_dict( img_dict ):
     features_dict = {}
+    prog = 0
+    dic_size = 0
     for key in img_dict.keys():
+        dic_size+=1
+
+    for key in img_dict.keys():
+        
         images = img_dict[key]
         fts = np.array([features(x) for x in images])
         features_dict[key] = fts
+        prog += 1
+        print("Progress dictionary building: {:.2f}%".format((prog/dic_size)*100))
     return features_dict
 
 """
 returns a vector (m x 1) with each row being the cluster label of each row in image_feat(m x 128) from clusters(k x 128)
 """
-def distance_cluster( image_feat, clusters ):
+def distance_cluster( image_feats, clusters ):
     X = []
     len_clusters = clusters.shape[0]
     """
     For each row, calculate its distance to each k-cluster and determine the k-cluster with the smallest distance
     and build a matrix of these k-clusters
     """
-    for row in range(0, image_feat.shape[0]):
-        e = image_feat[row,:]
+    #for row in range(0, image_feat.shape[0]):
+    for e in image_feats:
+
+        #e = image_feat[row,:]
         row_matrix = np.tile(e, (len_clusters,1))
         distances = paired_distances(row_matrix, clusters)
         X.append(np.argmin(distances))
@@ -95,6 +128,12 @@ def train_classifiers( labels, X, y):
 
         X_train, y_train = shuffle( X_train, y_train, random_state=0)
         svm = SVC(probability=True)
+
+        #Doing some CV grid search to find an optimal C 
+        # parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+        # clf = GridSearchCV(svm, parameters)
+        # clf.fit(X_train, y_train)
+
         svm.fit( X_train, y_train)
         acc = svm.score( X_train, y_train )
         print("Train score for {:} vs Non-{:} SVM classifier; accuracy: {:.2f}%".format(label, label, acc * 100))
@@ -121,6 +160,20 @@ def make_prediction( input_vec, classifiers ):
     # print("Largest probab = " + str(largest_prob))
     return prediction
 
+# Should create a dictionary with n random features per image for each label
+def random_features_perImage( img_dict):
+    n=10
+    rand_features_dict = {}
+    for key in img_dict.keys():
+        X=[]
+        for image in img_dict[key]:
+            feats=features(image)
+            randfeats = random.shuffle(feats)
+            X.append(randfeats[0:n])
+        fts = np.array(X)
+        rand_features_dict[key] = fts
+    return rand_features_dict
+
 if __name__ == "__main__":
     dic, count = build_dictionary("./training") 
     print("Calculating the features for " + str(count) + " images")   
@@ -131,15 +184,23 @@ if __name__ == "__main__":
         print("Loading precomputed clusters ...")
         kmeans = joblib.load('pt2_clusters.pkl')
     else:  
-        print("Computing clusters using kmeans ...")  
+        print("Building input matrix ...")  
+
+        #TODO: Random sampling for k-Means, not all features from training images (just 10 patches)
         X = []
+        prog = 0
+        ft_dict_size = 0
+        for images in list(ft_dict.values()):
+            ft_dict_size+=1
         for images in list(ft_dict.values()):
             entry = np.concatenate(images)
             X.extend(entry)
+            prog+=1
+            print("Progress building matrix: {:.2f}%".format((prog/ft_dict_size)*100))
+
         X = np.array(X)
         print("Input data shape = {:}".format(X.shape))   
-        #TODO: Preprocess X, normalise(X) as specified in assignment
-        kmeans = KMeans(n_clusters=100, n_jobs=-1) .fit(X) #TODO: Hack should be 500 clusters
+        kmeans = KMeans(n_clusters=500, n_jobs=2, verbose=True ).fit(X)
         joblib.dump( kmeans, 'pt2_clusters.pkl')
 
     X = []
@@ -154,15 +215,16 @@ if __name__ == "__main__":
     
     classifiers = train_classifiers( ft_dict.keys(), X, y)
     
-    img_paths = paths.list_images("./testing")
+    #TODO change to 'testing' when finished everything else
+    img_paths = paths.list_images("./testing_labelled")
     # img_paths = list(img_paths)[:2]
     clusters = kmeans.cluster_centers_
 
     with open('./run2.txt', 'a') as f:
         for img_path in img_paths:
-            label = img_path.split( os.path.sep )[2]
+            label = img_path.split( os.path.sep )[1]
             # print(label) #Visual Verification only
-            image = cv2.imread(img_path)
+            image = cv2.imread(img_path,0)
             fts = features( image )
             ft_vec = bag_of_words( fts, clusters)
             pred_label = make_prediction( ft_vec, classifiers)            
